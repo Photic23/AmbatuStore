0b48ada7b619b287360562aaed236733bafb6568
Photic23 <resellcourtesyx@gmail.com>
2023-12-02 17:31:31 +0700

    tes again?

--- AmbatuStore/wsgi.py
+++ AmbatuStore/wsgi.py
@@ -13,4 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AmbatuStore.settings")
 
+import django
+django.setup()
+
+from django.core.management import call_command
 application = get_wsgi_application()
